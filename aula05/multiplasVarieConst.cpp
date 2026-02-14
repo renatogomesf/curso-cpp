7e2ad97d86514bcfe5cbad4b2dc43f1a03888ae0
renatogomesf <renato_gfe@hotmail.com>
2026-02-13 21:23:48 -0300

    correção na aula

--- aula05/multiplasVarieConst.cpp
+++ aula05/multiplasVarieConst.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+// para definir uma constante, usa-se o "#define".
+// declaração: "#define", nome da constante em maiúsculo "PI2" e atribuir o valor sem uso do sinal igual "=".
+#define PI2 3.141516
+#define TEXTO "teste"
+
+// posso colocar comandos nas constantes
+#define CANAL std::cout << "canal fessor bruno \n";
+
 int main()
 {
 
@@ -20,6 +28,11 @@ int main()
     int vidas = 3, tiros = 500, life = 100;
 
     // CONSTANTES ----------
+    cout << PI2 << "\n";
+    cout << TEXTO << "\n";
+
+    // e no caso de constantes com comandos, basta chamar a constante que ela executa o comando.
+    CANAL
 
     // para escrever uma variável que não vai mudar o valor, ou seja, será constante, basta escrever "const" na frente.
     // por convenção, constantes são escritaas em MAIÚSCULO.
