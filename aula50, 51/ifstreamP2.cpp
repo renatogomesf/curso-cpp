06a6d73a6d3fd4a9b1f79650fb5a4572806febb9
renatogomesf <renato_gfe@hotmail.com>
2026-02-18 17:15:11 -0300

    aula50 e 51

---
+++ aula50, 51/ifstreamP2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+// Operações com arquivos
+// para trabalhar com arquivos, usa-se a biblioteca "fstream"
+#include <fstream>
+
+int main()
+{
+
+    // ofstream "Out File Stream -> OFStram": define o arquivo como tipo siada. partindo do nosso sistema/programa e vai pro arquivo
+    // ifstream "In File Stream -> IFStram": define o arquivo como tipo entrada. partindo do arquivo e vindo pro sistema/programa
+    // fstream "File Stream -> FSstream": pode ser definido como entrada e saida.
+
+    // variável definida/tipada como entrada.
+    std::ifstream arquivoE;
+
+    // para ler o conteúdo do arquivo precisa do "while" para poder ler linha por linha e o método "getline" para obter a linha do arquivo e mostrar na tela.
+    // armazena a linha lida para mostrar na tela com "cout".
+    std::string linha;
+
+    // "open()": abre o arquivo passando o nome e extenção. abre ele para poder ler o conteúdo.
+    arquivoE.open("cfbcursos.txt");
+
+    // "is_open()": verifica se o arquivo está aberto mesmo. caso esteja, retorna TRUE. caso não ou não exista, retorna FALSE
+    if (arquivoE.is_open())
+    {
+        // "getline()": lê linha por linha dor arquivo. pede dois parâmetros... um é o arquivo que vai ser lido e a outra é onde ele vai colocar o que foi lido na linha.
+        while (getline(arquivoE, linha))
+        {
+            std::cout << linha << "\n";
+        }
+
+        arquivoE.close();
+    }
+    else
+    {
+        std::cout << "não foi possível abrir o arquivo.\n";
+    }
+
+    return 0;
+}
---
+++ aula50, 51/ofstreamP1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// Operações com arquivos
+// para trabalhar com arquivos, usa-se a biblioteca "fstream"
+#include <fstream>
+
+int main()
+{
+
+    // ofstream "Out File Stream -> OFStram": define o arquivo como tipo siada. partindo do nosso sistema/programa e vai pro arquivo
+    // ifstream "In File Stream -> IFStram": define o arquivo como tipo entrada. partindo do arquivo e vindo pro sistema/programa
+    // fstream "File Stream -> FSstream": pode ser definido como entrada e saida.
+
+    // variável definida/tipada como saida
+    std::ofstream arquivo;
+
+    // "open()": cria/abre o arquivo passando o nome e extenção. gera o arquivo caso não exista... e abre ele, caso já exista, para poder adicionar dados.
+    // "ios::app": sem ele o aquivo gerado tem seu conteúdo sobrescrito sempre que for adicionado algo. usando "ios::app" o conteúdo não é sobrescrito... o cursor vai pro final do conteúdo ja existen no arquivo e adiciona o novo.
+    arquivo.open("cfbcursos.txt", std::ios::app);
+
+    // para colocar dados no arquivo basta usar a sintax do "cout << 'texto'"
+    arquivo << "CFB Cursos\n";
+    arquivo << "Cursos de C++\n";
+    arquivo << "cfbcursos.com.br\n";
+    arquivo << "youtube.com/cfbcursos\n";
+
+    // "close()": fecha o arquivo. sempre importante fechar o arquivo depois de fazer as operações com ele para liberar memória.
+    arquivo.close();
+
+    return 0;
+}
