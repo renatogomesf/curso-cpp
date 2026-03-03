e95b5a703540af595d9d711d97d3750f0fc18385
Renato Gomes <renato_gfe@hotmail.com>
2026-03-03 18:22:48 -0300

    aula63

---
+++ aula63/forEach.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+// para trablhar com for each, precisa desta biblioteca
+#include <algorithm>
+
+int main()
+{
+
+    std::vector<int> n{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    // for range é mais pratico.
+    // parecido com o map. passa item por item para a função lambda definida.
+    // declaração: std::for_each("início da estrutura", "fim da estrutura", []("parametro que recebe cada elemento"){});
+    std::for_each(n.begin(), n.end(), [](int elemento){ 
+
+        // as operação e mudanças só ocorrem dentro da função lambda. o vector não é alterado.
+        elemento += 10;
+
+        std::cout << elemento << "\n";
+    });
+
+    return 0;
+}
